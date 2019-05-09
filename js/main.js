f33d114ac66753f499d1b8849e9246f80080cef3
Viktor Ivanchenko <viittyok1992@gmail.com>
2019-05-10 00:18:02 +0300

    fixed and added styles for headers and first text block

--- js/main.js
+++ js/main.js
@@ -3,4 +3,13 @@ $(document).ready(function(){
 		$('#menu-hamburger').toggleClass('opened');
 		$('.top-menu nav > ul').toggleClass('opened');
 	});
+	let topMenuPosition = $('header > .top-menu').offset();
+	$(window).scroll(function(){
+	    if ($(window).scrollTop() >= topMenuPosition.top) {
+	        $('header > .top-menu').addClass('sticky');
+	    }
+	    else {
+	        $('header > .top-menu').removeClass('sticky');
+	    }
+	});
 });
