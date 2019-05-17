e45513700fdae6c8916dfeb8f23dbe74be87f916
Viktor Ivanchenko <viittyok1992@gmail.com>
2019-05-17 21:02:29 +0300

    added styles and script for advantages transitions

--- js/main.js
+++ js/main.js
@@ -4,9 +4,17 @@ $(document).ready(function(){
 		$('.top-menu nav > ul').toggleClass('opened');
 	});
 	let topMenuPosition = $('header > .top-menu').offset();
+	let advantagesBlockPosition = $('.advantages-sect').offset();
 	$(window).scroll(function(){
-	    if ($(window).scrollTop() >= topMenuPosition.top) {
+		let scrollPosition = $(window).scrollTop()
+	    if (scrollPosition >= topMenuPosition.top) {
 	        $('header > .top-menu').addClass('sticky');
+	        if (scrollPosition >= advantagesBlockPosition.top) {
+		        $('.advantages-sect .advantage-item').addClass('item-visible'); 
+		    }
+		    else {
+		        $('.advantages-sect .advantage-item').removeClass('item-visible');
+		    }
 	    }
 	    else {
 	        $('header > .top-menu').removeClass('sticky');
